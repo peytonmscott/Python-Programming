f1e20e449eb57dbe922ea43833f1fd097fffd006
Peyton Scott <pmscott@Peytons-MacBook-Pro.local>
2022-03-12 09:40:53 -0600

    Completed the project.

--- todo_console_ui.py
+++ todo_console_ui.py
@@ -1,30 +1,47 @@
-import datetime
+from datetime import datetime
 
-task1 = (True, "CPSC 4970 Hw", "2022-03-013 23:59")
-task2 = (False, "Laundry", "2022-03-06 12:15")
-task3 = (True, "Send email to Rick", "2022-03-09 8:00")
-todo = [task1, task2, task3]
-
-
-
-print("Main menu: ")
-print("1) List ALL todo items ")
-print("2) List all incomplete todo items ")
-print("3) List incomplete todo items due today ")
-print("4) Add a todo item ")
-print("5) Complete a todo item ")
-print("6) Quit ")
-selection = input("Enter your choice> ")
-
-
-if int(selection) == 1: print(todo)
-
-# if int(selection) == 2:
-
-# if int(selection) == 3:
-
-# if int(selection) == 4:
-
-# if int(selection) == 5:
-
-# if int(selection) == 6:
+lst = []
+while (1): # Infinite loop to print the menu again and again till quit option is not choosen
+    print("Main Menu : ")
+    print("1. List all incomplete todo items")
+    print("2. List incomplete todo items due today")
+    print("3. Add a todo item")
+    print("4. Complete a todo item")
+    print("5. Quit")
+    
+    selection = int(input("Enter your selection > "))
+    
+    if selection == 1:
+        print("\nIncomplete items:")
+        for tpl in lst:
+            print(f"\t{tpl[0]} due {str(tpl[1])}")
+    elif selection == 2:
+        print("\nIncomplete items due today:")
+        CurrentDate = datetime.now()
+        for tpl in lst:
+            if tpl[1].date() == CurrentDate.date():
+                print(f"\t{tpl[0]} due {str(tpl[1])}")
+    elif selection == 3:
+        desc = input("\nEnter item description : ")
+        duedate = input("Enter due date/time (MM/DD/YYYY hh:mm) : ")
+        format = "%m/%d/%Y %H:%M"
+        duedate = datetime.strptime(duedate, format)
+        tpl = (desc, duedate)
+        print(tpl)
+        lst.append(tpl)
+    elif selection == 4:
+        if len(lst) != 0:
+            print("\nIncomplete items:")
+            x = 0
+            for tpl in lst:
+                print(f"\t{x}) {tpl[0]} due {str(tpl[1])}")
+                x += 1
+            delete = int(input("\nEnter item to complete : "))
+            del lst[delete]
+        else:
+            print("No incomplete todo items available.")
+    elif selection == 5:
+        print("Bye!")
+        exit()
+    else:
+        print("\n")
